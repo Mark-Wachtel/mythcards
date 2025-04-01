0d7ead759747f26925b8d086b953eac6de38519a
Mark-Wachtel <info@wachtel1.de>
2025-04-01 15:49:03 +0200

    init commit

---
+++ mythcardsgame/client/src/main/java/client/ClientMain.java
@@ -0,0 +1,23 @@
+package client;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class ClientMain extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setTitle("Java Kartenspiel Client");
+        settings.setWidth(1280);
+        settings.setHeight(720);
+    }
+
+    @Override
+    protected void initGame() {
+        // Hier deine FXGL-Spielinitialisierung
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
